76f85700a782be0b67a06d099bfd07d4129adaff
anup.chhetri <anup007ece@gmail.com>
2021-01-26 17:35:57 +0530

    Added put and delete mapping for beer controller.

--- src/main/java/com/anup/breweryservice/services/BeerServiceImpl.java
+++ src/main/java/com/anup/breweryservice/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.anup.breweryservice.services;
 
 import com.anup.breweryservice.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 @Service
+@Slf4j
 public class BeerServiceImpl implements BeerService {
     @Override
     public BeerDto getBeerById(UUID beerId) {
@@ -21,5 +23,15 @@ public class BeerServiceImpl implements BeerService {
                 .beerName("Tuborg")
                 .beerStyle("Medium")
                 .build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        // add real impl to update beer
+    }
+
+    @Override
+    public void deleteByID(UUID beerId) {
+        log.debug("Deleting a beer");
     }
 }
